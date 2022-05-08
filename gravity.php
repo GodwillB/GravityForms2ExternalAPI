897afcb99e43d79c86e45e1a0ea00d55e96d02cc
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2022-05-08 03:16:19 +0300

    update source file

---
+++ gravity.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name: Gravity Rest
+ * Plugin URI: https://github.com/GodwillB
+ * Author: Godwill Barasa
+ * Author URI: https://github.com/GodwillB
+ * Description: Collect Gravity forms to Webhook
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: gravity-rest
+*/
+
+defined( 'ABSPATH' ) or die( 'No entry' );
+
+add_action( 'gform_after_submission_2', 'techiepress_after_submission_2', 10, 2 );
+
+function techiepress_after_submission_2( $entry, $form ) {
+
+  
+
+    if ( $entry['form_id'] == 2 ) {
+
+        $info = [
+            'name'   => $entry[1],
+            'phone' => $entry [2],
+            'email'  => $entry[3],
+            'apartmentType'  => $entry[6],
+            'url' => $entry[7],
+        ];
+
+        $url = 'https://crm.mividahomes.com/api/enquiries';
+
+        $args = [
+            'method' => 'POST',
+            'body'   => json_encode($info),
+            'headers'     => array('Content-Type' => 'application/json; charset=utf-8'),
+            'data_format' => 'body',
+        ];
+
+        $post = wp_remote_post( $url, $args );
+
+
+        error_log(print_r( $post, true ));
+
+    }
+
+}
+
+add_action( 'gform_after_submission_1', 'techiepress_after_submission_1', 10, 2 );
+
+function techiepress_after_submission_1( $entry, $form ) {
+
+    // error_log( print_r( $entry, true ) );
+
+    if ( $entry['form_id'] == 1 ) {
+
+        $info = [
+            
+            'email'  => $entry[1],
+			'url' => $entry[2],
+           
+        ];
+
+        $url = 'https://crm.mividahomes.com/api/popupemail';
+
+        $args = [
+            'method' => 'POST',
+            'body'   => json_encode($info),
+            'headers'     => array('Content-Type' => 'application/json; charset=utf-8'),
+            'data_format' => 'body',
+        ];
+
+         $post = wp_remote_post( $url, $args );
+
+
+        error_log(print_r( $post, true ));
+
+    }
+
+}
